e6b278d184378b2ddf331cff976fb0c3b6fccb9d
ShuliangLu <graphicslsl@gmail.com>
2019-12-04 16:56:45 +0800

    basis_structure

---
+++ cmake-build-debug/src/config/CMakeFiles/configuration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/configuration.dir/Configuration.cpp.o"
+  "../../../lib/libconfiguration.pdb"
+  "../../../lib/libconfiguration.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/configuration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/utility/reducedSpaceSimulator.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is the reduced space simulator" << std::endl;
+}
